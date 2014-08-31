846740b7fd59bc6f62d94d9f4db7f5e4236c05d2
Lia Bogoev <lia.bogoev@gmail.com>
2014-08-30 21:34:53 -0600

    Adds .gitignore, basic Assign1.cpp structure to take command line arguments and cout those arguments

---
+++ Assign1.cpp
@@ -0,0 +1,13 @@
+// Lia Bogoev 
+// CS3100 - Assign 1
+
+#include <iostream>
+
+int main(int argc, char* argv[]){
+	for (int arg = 0; arg < argc; arg++) {
+		std::cout << argv[arg] << std::endl;
+	}
+
+
+	return 0;
+}
