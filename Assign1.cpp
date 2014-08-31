71a582ed1c503efedecc3f335217ee31f3804833
Lia Bogoev <lia.bogoev@gmail.com>
2014-08-31 12:34:10 -0600

    Sorts input, displays menu if anything other than 2 arguments

--- Assign1.cpp
+++ Assign1.cpp
@@ -5,33 +5,30 @@
 
 int main(int argc, char* argv[]){
 
-	//std::cout << "argc: " << argc << "\n";
+	if(argc == 3){
+		// correct number of arguments
 
-	if(argc == 1){
-		// No argument input
-		std::cout << " --- Assign1 Help ---\n";
-		std::cout << " -fib [n] Compute the fibonacci of [n]\n";
-		std::cout << " -e [n]   Compute the value of 'e' using [n] iterations\n";
-		std::cout << " -pi [n]   Computer pi to [n] digits\n";
-	} // end if(argc == 0)
+		if(std::string(argv[1]) == "-fib"){
+			std::cout << "fib\n";
+		} // end == "-fib"
 
-
-
-/*
-	for (int arg = 0; arg < argc; arg++) {
-		//std::cout << arg  << std::endl;
-	
-		if(std::string(argv[1]) == "-f"){
- 			std::cout << "fib\n";
-		}
 		else if(std::string(argv[1]) == "-e"){
-			std::cout << "the number e\n";
-		}
-		else{
-			std::cout << "....else\n";
-		}
+			std::cout << "eeeee\n";
+		} // end == "-e"
+
+	 	else if(std::string(argv[1]) == "-pi"){
+			std::cout << "pie\n";
+		} // end == "-pi"
 	}
-*/
+
+	else{
+		// No argument input or incorrect input 
+		std::cout << "  --- Assign1 Help ---\n";
+		std::cout << "  -fib [n] Compute the fibonacci of [n]\n";
+		std::cout << "  -e [n]   Compute the value of 'e' using [n] iterations\n";
+		std::cout << "  -pi [n]  Compute pi to [n] digits\n";
+	} // end else 
+
 
 	return 0;
 }
