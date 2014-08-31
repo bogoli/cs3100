2e818b9c60dd7622340c1b920a99da952045948e
Lia Bogoev <lia.bogoev@gmail.com>
2014-08-30 22:30:39 -0600

    Adds a menu when no arguments

--- Assign1.cpp
+++ Assign1.cpp
@@ -4,10 +4,34 @@
 #include <iostream>
 
 int main(int argc, char* argv[]){
-	for (int arg = 0; arg < argc; arg++) {
-		std::cout << argv[arg] << std::endl;
-	}
 
+	//std::cout << "argc: " << argc << "\n";
+
+	if(argc == 1){
+		// No argument input
+		std::cout << " --- Assign1 Help ---\n";
+		std::cout << " -fib [n] Compute the fibonacci of [n]\n";
+		std::cout << " -e [n]   Compute the value of 'e' using [n] iterations\n";
+		std::cout << " -pi [n]   Computer pi to [n] digits\n";
+	} // end if(argc == 0)
+
+
+
+/*
+	for (int arg = 0; arg < argc; arg++) {
+		//std::cout << arg  << std::endl;
+	
+		if(std::string(argv[1]) == "-f"){
+ 			std::cout << "fib\n";
+		}
+		else if(std::string(argv[1]) == "-e"){
+			std::cout << "the number e\n";
+		}
+		else{
+			std::cout << "....else\n";
+		}
+	}
+*/
 
 	return 0;
 }
