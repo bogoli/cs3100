4495071c444932b2ed50faee4eb5319979aae0ad
Lia Bogoev <lia.bogoev@gmail.com>
2014-09-01 16:42:36 -0600

    Adds try catch blocks and command line argument structure

--- Assign1.cpp
+++ Assign1.cpp
@@ -2,32 +2,57 @@
 // CS3100 - Assign 1
 
 #include <iostream>
+#include <exception>
+#include <new>
 
 int main(int argc, char* argv[]){
+	int par;
 
 	if(argc == 3){
 		// correct number of arguments
 
 		if(std::string(argv[1]) == "-fib"){
-			std::cout << "fib\n";
+			try{
+				par = std::stoi(argv[2]);
+			} catch(...){
+				std::cout << "Invalid Parameter\n";	
+			} // end try catch block 
+
+		// calculate fibonacci here (0-14 inclusive)
+
 		} // end == "-fib"
 
 		else if(std::string(argv[1]) == "-e"){
-			std::cout << "eeeee\n";
-		} // end == "-e"
+			try{
+				par = std::stoi(argv[2]);
+			} catch(...){
+				std::cout << "Invalid Parameter\n";	
+			} // end try catch block 
+		
+		// calculate e here (1-100 iterations)
 
-	 	else if(std::string(argv[1]) == "-pi"){
-			std::cout << "pie\n";
+		} // end == "-e"
+	 	
+
+		else if(std::string(argv[1]) == "-pi"){
+			try{
+				par = std::stoi(argv[2]);
+			} catch(...){
+				std::cout << "Invalid Parameter\n";	
+			} // end try catch block 
+			
+			// calulate pi here (1-10 inclusive)
+
 		} // end == "-pi"
-	}
+	}// end if there are the correct number of inputs
 
 	else{
 		// No argument input or incorrect input 
-		std::cout << "  --- Assign1 Help ---\n";
-		std::cout << "  -fib [n] Compute the fibonacci of [n]\n";
-		std::cout << "  -e [n]   Compute the value of 'e' using [n] iterations\n";
-		std::cout << "  -pi [n]  Compute pi to [n] digits\n";
-	} // end else 
+		std::cout << "--- Assign1 Help ---\n";
+		std::cout << "-fib [n] Compute the fibonacci of [n]\n";
+		std::cout << "-e [n]   Compute the value of 'e' using [n] iterations\n";
+		std::cout << "-pi [n]  Compute pi to [n] digits\n";
+	} // end else (incorrect number of inputs)
 
 
 	return 0;
