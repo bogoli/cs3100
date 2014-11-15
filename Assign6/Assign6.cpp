242b91b686dd7854ce72adf927b34b15cab795bc
Lia Bogoev <lia.bogoev@gmail.com>
2014-11-15 11:59:11 -0700

    Removed completely superfluous lerp function I was testing. Whoops.

--- Assign6/Assign6.cpp
+++ Assign6/Assign6.cpp
@@ -8,22 +8,6 @@
 
 const double CONVERSION_FACTOR = 0.0174532925;
 
-double lerp(double deg, double* trigArray){
-	int x0 = floor(deg);
-	double y0 = trigArray[x0];
-	int x1 = ceil(deg);
-	double y1 = trigArray[x1];
-	// double x ---> deg
-
-	// find the slope
-	double m = (y1 - y0) / (x1 - x0);
-	// find the y intercept
-	double b = -m*x0 + y0;
-	// plug in the degree value into the equation
-	double y = m * deg + b;  // deg is the x that we want to plug into the equation
-	return y;
-}
-
 int main(){
 	// open the library
 	void* handle = dlopen("./trig-lookup.so", RTLD_LAZY);
@@ -53,9 +37,6 @@ int main(){
 		exit(1);
 	}
 
-	std::cout << "mysin(45.2): " << mysin(45.2) << std::endl;
-	std::cout << "mycos(45.2): " << mycos(45.2) << std::endl;
-	std::cout << "mytan(42.5): " << mytan(42.5) << std::endl;
 
 
 
