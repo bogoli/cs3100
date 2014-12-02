c8b83e443287ef562df1a810868a59fe36dc8ec7
Lia Bogoev <lia.bogoev@gmail.com>
2014-12-01 21:14:25 -0700

    Adds make_sequences function prototype.

--- Assign7/Assign7.cpp
+++ Assign7/Assign7.cpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #include <random>
-#include <unordered_map>
+#include <array>
 
 std::default_random_engine generator(time(NULL));
 
@@ -12,8 +12,20 @@ const int PAGE_SIZE = 250;	// max value of page
 const int MEM_SIZE = 100;	// number of frames to test
 const int NUM_SEQ = 100; 	// number of sequences to run
 
-typedef std::unordered_map<int,int> intmap;
-intmap memory;
+typedef array<int, 1000> page_sequence;
+
+array<page_sequence, NUM_SEQ> make_sequences(){
+	// make an array of arrays of 1000
+	array<page_sequence, NUM_SEQ> test_sequences;
+
+	for(int i = 0; i < NUM_SEQ; ++i){
+		for (int j = 0; j < SEQ_SIZE; ++j){
+			test_sequences<i,j>  = rand()%PAGE_SIZE;
+		}
+	}
+		
+	return test_sequences;
+}
 
 int main(){
 /*
@@ -22,32 +34,23 @@ int main(){
 	std::cout << "Frames of physical memory: " << MEM_SIZE << std::endl;
 */
 
-	// value of my memory array is the key of my page array, and vice versa
-	// value of page array is key of memory array
+	/* Create a list of 100 random page sequences 
+      and save it so we can use the exact same 
+      sequences for each page size
+ 	*/
+ 	// array<page_sequence, 100> test_sequences = make_sequences();
 
-	int sequence[SEQ_SIZE];
+	// We'll eventually need to save the test results somewhere results is that somewhere
+	// 	auto results = //Something something
 
-	for (int i = 0; i < SEQ_SIZE; ++i){
-		// fill an array (page sequence)
-		sequence[i] = rand()%PAGE_SIZE;
+	// for each of the frame sizes we need to test
+	for(int frames = 1; frames <= 100; frames++){
+		// Do some tests and insert them into the results
+		// results.Add(do_test(page_size, tests))
 	}
 
-
-//	for(int i = 0; i < MEM_SIZE; ++i){
-	
-	int memory[100];
-	int next = 0;
-	int page_faults = 0;
-		for(int j = 0; j < 1000; ++ j){
-			// if in memory, do nothing
-			
-			// if not in memory:
-				memory[next] = sequence[j];
-				++page_faults;
-		
-		}
-//	}
-
+	//print_results(results)
+	//}
 
 	return 0;
 }
