78ba6d9185ae5004672764690d3b27ba6501ec38
Lia Bogoev <lia.bogoev@gmail.com>
2014-12-01 23:10:47 -0700

    Something. not working.

--- Assign7/Assign7.cpp
+++ Assign7/Assign7.cpp
@@ -6,14 +6,12 @@
 #include <array>
 #include <deque>
 
-std::default_random_engine generator(time(NULL));
-
 const int SEQ_SIZE = 1000;	// size of page sequence
 const int PAGE_SIZE = 250;	// max value of page
 const int MEM_SIZE = 100;	// number of frames to test
 const int NUM_SEQ = 100; 	// number of sequences to run
 
-typedef std::array<int, 1000> page_sequence;
+typedef std::array<int, SEQ_SIZE> page_sequence;
 
 std::array<page_sequence, NUM_SEQ> make_sequences(){
 	// make an array of arrays of 1000
@@ -43,36 +41,46 @@ int do_test(int frames, std::deque<int>& memory, page_sequence& sequence){
 			page_faults++;
 			memory.pop_front();
 			memory.push_back(page);
+
 		}
 	}
 	return page_faults;
 }
 
 
-void setup_test(int size, std::array<page_sequence, 1000>& test_sequences){
+void setup_test(int size, std::array<page_sequence, NUM_SEQ>& test_sequences){
 	// allocate our "memory"
 	auto memory = std::deque<int>(); 
+	int page_faults = NUM_SEQ + 1;
 
 	// iterate through the different page sequences
-	for(int i = 0; i < test_sequences.size(); ++i){
-		do_test(size, memory, test_sequences[i]);
+	for(int i = 0; i < test_sequences.size(); ++i){	
+		if(do_test(size, memory, test_sequences[i]) > page_faults){
+		
+			std::cout << "Anomaly Detected: " << std::endl;
+			std::cout << "Sequence: " << i << std::endl;
+			std::cout << "Number of Frames: " << size << std::endl;
+			std::cout << "Page_faults " << page_faults << std::endl;
+			std::cout << "- - - - - - - - - - - - - \n";
+		
+			page_faults = do_test(size, memory, test_sequences[i]);
+		}
+
+		else{
+			page_faults = do_test(size, memory, test_sequences[i]);
+		}
 	}
 }
 
 
 int main(){
-/*
+
 	std::cout << "Number of sequences tested: " << NUM_SEQ << std::endl;
 	std::cout << "Length of page usage sequence: " << SEQ_SIZE << std::endl;
 	std::cout << "Frames of physical memory: " << MEM_SIZE << std::endl;
-*/
+	std::cout << std::endl << std::endl << std::endl;
 
-	/* 
-	Create a list of 100 random page sequences 
-    and save it so we can use the exact same 
-    sequences for each page size
- 	*/
- 	std::array<page_sequence, 100> test_sequences = make_sequences();
+ 	std::array<page_sequence, NUM_SEQ> test_sequences = make_sequences();
 
 /*
  	for(int i = 0; i < NUM_SEQ; ++i){
@@ -83,14 +91,9 @@ int main(){
 	}
 */
 
-	// We'll eventually need to save the test results somewhere results is that somewhere
-	// 	auto results = //Something something
-
 	// for each of the frame sizes we need to test
-	for(int frames = 1; frames <= 100; frames++){
-
-		// Do some tests and insert them into the results
-		// results.Add(do_test(page_size, tests))
+	for(int frames = 1; frames <= MEM_SIZE; frames++){
+		setup_test(frames, test_sequences);
 	}
 
 	//print_results(results)
